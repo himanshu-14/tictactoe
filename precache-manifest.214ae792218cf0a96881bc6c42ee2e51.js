1ea0fa26044382536550ad66952044825e9ff27f
Himanshu Mittal <himanshu1495@gmail.com>
2020-04-09 03:29:58 +0530

    added build files to production branch

---
+++ precache-manifest.214ae792218cf0a96881bc6c42ee2e51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa962c8b6f67b76f20ab25e301d577ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb95775264a715c416ca",
+    "url": "/static/css/main.a9827d45.chunk.css"
+  },
+  {
+    "revision": "41ef3859d7e645822cc1",
+    "url": "/static/js/2.2c1ec1bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c1ec1bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb95775264a715c416ca",
+    "url": "/static/js/main.be1e4d43.chunk.js"
+  },
+  {
+    "revision": "352986e75333c2d5e45a",
+    "url": "/static/js/runtime-main.a6572006.js"
+  }
+]);
